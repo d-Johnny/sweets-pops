376f7e413121feb64abf943884f009cd11f3fc85
Johnny <Komarnitskii@gmail.com>
2018-07-13 09:36:33 +0300

    error

---
+++ js/1script.js
@@ -0,0 +1,32 @@
+
+
+// function keks (){
+//   document.getElementById('keks').innerHTML = "добро пожаловать";
+// }
+
+
+// function text (){
+//   document.getElementById('text').innerHTML = "красавчик нажал";
+// }
+
+// function turnOff() {
+//   document.getElementById('myImage').src='img/pops2.png';
+// }
+// function turnOn() {
+//   document.getElementById('myImage').src="img/pops1.png";
+// }
+
+// function newContent() {
+//   alert("load new content");
+//   document.open();
+//   document.write("<h1>Document write (новый)</h1><p>Новый контент</p>");
+//   document.close();
+// }
+
+function showAlert(){
+  var myInput = document.getElementById('my_input_name');
+  console.log('myInput', myInput);
+  var inputValue = myInput.value;
+  console.log('inputValue', inputValue);
+  alert(inputValue);
+}
